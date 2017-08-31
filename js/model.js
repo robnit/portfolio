d0380361c7f99a5ff3422045a3cdd055b01c43f2
Robin Miller <rmillerpdx@gmail.com>
2017-08-30 17:14:32 -0700

    add projects.js file

--- js/model.js
+++ js/model.js
@@ -1,6 +1,8 @@
-function Project (title, date, summary, imageUrl) {
+'use strict';
+
+function Project (title, url, summary, imageUrl) { // eslint-disable-line
     title = this.title;
-    date = this.date;
+    url = this.url;
     summary = this.summary;
     imageUrl = this.imageUrl;
 }
