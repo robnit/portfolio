48de508024b59aa47248b98597ee1456872b0bd6
Robin Miller <rmillerpdx@gmail.com>
2017-09-05 08:56:39 -0700

    create nonfunctioning hide/show tab feature

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 'use strict';
 
+$('.tab:contains(Home)').on('click',function(event){
+    (event).preventDefault();
+    ('#projects').show();
+    ('#about').hide();
+})
+
+$('.tab:contains=(About)').on('click',function(event){
+    (event).preventDefault();
+    ('#projects').hide();
+    ('#about').show();
+})
