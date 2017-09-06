b58e452388ada9b62d9e7318fc6c64e2a6bd1cba
Robin Miller <rmillerpdx@gmail.com>
2017-09-05 17:48:06 -0700

    fix preventdefault

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,14 @@
 'use strict';
 
 $('.tab:contains(Home)').on('click',function(event){
-    // $(event).preventDefault();
+    event.preventDefault();
     console.log('hi');
     $('#projects').show();
     $('#about').hide();
 })
 
 $('.tab:contains(About)').on('click',function(event){
-    // $(event).preventDefault();
+    event.preventDefault();
     console.log('hello');
     $('#projects').hide();
     $('#about').show();
