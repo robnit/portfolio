10dbbef9f3ec353233149d67c64bd3b432512429
Robin Miller <rmillerpdx@gmail.com>
2017-09-05 17:42:32 -0700

    get tabs working

--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,15 @@
 'use strict';
 
+$('.tab:contains(Home)').on('click',function(event){
+    // $(event).preventDefault();
+    console.log('hi');
+    $('#projects').show();
+    $('#about').hide();
+})
+
+$('.tab:contains(About)').on('click',function(event){
+    // $(event).preventDefault();
+    console.log('hello');
+    $('#projects').hide();
+    $('#about').show();
+})
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 'use strict';
 
-const express = require( 'express' ); //makes it so error is returned if express module is not found
+const express = require( 'express' );
 const app = express ();
-const PORT = process.env.PORT || 3000;
+const MYPORT = process.env.MYPORT || 3000;  //process.env returns object containing user environment
 
 app.use( express.static('./public') );
 
 app.get( '/', function( request,response ){
-  response.sendFile( 'index.html', { root: './public' } );
+    response.sendFile( 'index.html', { root: './public' } );
 });
 
-app.listen(PORT, function() {
-    console.log(`it's working! port is ${PORT}`)
-  });
+app.listen(MYPORT, function() {
+    console.log(`it's working! port is ${MYPORT}`)
+});
